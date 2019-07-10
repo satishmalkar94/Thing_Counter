35a330361ad6155275410d8cee8700a6dd1ed438
satish <satishmalkar94@gmail.com>
2019-07-10 22:12:57 +0530

    Enhancement::update code

--- js/hello.js
+++ js/hello.js
@@ -1,27 +1,71 @@
-var fvalue
-var fcolor
+var counters = [];
+value =0;
 
-function Save() {
-          var fvalue = document.getElementById("textvalue").value;
-          alert(textvalue)
-          var fcolor = document.getElementById("cvalue").value
-          alert(cvalue)
-          document.getElementById("total1").innerHTML = fvalue;
-          // document.getElementById("total1").innerHTML = fcolor;
-          // alert(fcolor)
- };
-// -------------------------------------------------------------------------------
-count = 0;
-  function FunPlus() {
+function FunPlus(elementVal){
+  counters[elementVal]++;
+  document.getElementById(elementVal +"").innerHTML =counters[elementVal]
+}
 
-          count += 1;
-          document.getElementById("total").innerHTML = count;
+function FunMinus(elementVal){
+  counters[elementVal]--;
+  document.getElementById(elementVal +"").innerHTML = counters[elementVal]
+}
 
-  };
-// -----------------------------------------------------------------------------
-  function FunMinus() {
-    count -= 1;
-    // alert(count);
-     document.getElementById("total").innerHTML = count;
-  };
+function AddButton() {
+    counters.push(0);
 
+    var textVal = document.getElementById("textvalue").value;
+    var Color = document.getElementById("cvalue").value;
+    var newControl = "<div class='col-md-8'><div style='background-color:" + Color + "' class='addremove'><i class='fa fa-minus-circle plusi' onClick='FunMinus(" + value + ")'></i><span class='counter' ><label id='" + value + "' >0</label><label>" + textVal + "</label></span><i class='fa fa-plus-circle plusj' onClick='FunPlus(" + value + ")'></i></div></div>";
+
+    document.getElementById("totalbutton").innerHTML = document.getElementById("totalbutton").innerHTML + newControl;
+    value++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var counters = [];
+// value =0;
+
+// function FunPlus(elementVal){
+//   counters[elementVal]++;
+//   document.getElementById(elementVal +"").innerHTML =counters[elementVal]
+// }
+
+// function FunMinus(elementVal){
+//   counters[elementVal]--;
+//   document.getElementById(elementVal +"").innerHTML = counters[elementVal]
+// }
+
+// function AddButton() {
+//     counters.push(0);
+
+//     var textVal = document.getElementById("textvalue").value;
+//     var Color = document.getElementById("cvalue").value;
+//     var newControl = "<div class='col-md-8'><div style='background-color:" + Color + "' class='addremove'><i class='fa fa-minus-circle plusi' onClick='FunMinus(" + value + ")'></i><span class='counter' ><label id='" + value + "' >0</label><label>" + textVal + "</label></span><i class='fa fa-plus-circle plusj' onClick='FunPlus(" + value + ")'></i></div></div>";
+
+//     document.getElementById("totalbutton").innerHTML = document.getElementById("totalbutton").innerHTML + newControl;
+//     value++;
+// }
